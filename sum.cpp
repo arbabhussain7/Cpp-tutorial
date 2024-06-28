988b1b62b9a803328ee0ed554ad625753b5685b0
Arbab Hussain <arbabhussain414@gmail.com>
2024-06-28 11:58:09 +0500

    sum of two number

---
+++ sum.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1 , num2,sum;     // variable declaraion 
+    num1 =10;                // variable intilization
+    num2 = 15;
+    sum = num1 + num2;         // put formula 
+    cout<<" Sum of two number is "<<sum;    // output
+}
